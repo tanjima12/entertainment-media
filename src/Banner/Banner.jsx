e16300db2bca3b6f1fd18c93b80c210a85f3ac7a
tanjima12 <tanjimaakter005@gmail.com>
2023-12-14 23:38:04 +0600

    banner headline

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,9 +1,41 @@
 const Banner = () => {
+  anime
+    .timeline({ loop: true })
+    .add({
+      targets: ".ml15 .word",
+      scale: [14, 1],
+      opacity: [0, 1],
+      easing: "easeOutCirc",
+      duration: 800,
+      delay: (el, i) => 800 * i,
+    })
+    .add({
+      targets: ".ml15",
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000,
+    });
   return (
-    <div className="bannerbg h-[550px] rounded-lg mr-3 ml-3 lg:mr-10 lg:ml-10">
-      <h1 className="text-white font-orbitron text-5xl mt-10 pt-10 lg:pt-40 text-center">
-        Unlimited movies, TV shows, and more
+    <div className="bannerbg h-[550px] rounded-lg mr-3 ml-3 lg:mr-10 lg:ml-10 ">
+      <h1 className="ml15  text-white  mt-5  lg:pt-10 text-center ">
+        <span className="word bg-gradient-to-r from-[#B7F8DB] to-[#50A7C2] text-transparent bg-clip-text">
+          Unlimited{" "}
+        </span>
+        <span className="word bg-gradient-to-r from-[#48c6ef] to-[#6f86d6] text-transparent bg-clip-text">
+          Movies,{" "}
+        </span>
+        <span className="word bg-gradient-to-r from-[#13547a] to-[#80d0c7] text-transparent bg-clip-text">
+          TV shows{" "}
+        </span>
+
+        <span className="word bg-gradient-to-r from-[#667eea] to-[#764ba2] text-transparent bg-clip-text">
+          and more
+        </span>
       </h1>
+      {/* <h1 className="">
+        , , 
+      </h1> */}
       <p className="text-white font-poppins mt-2 text-xl mb-10 text-center">
         Watch anywhere. Cancel anytime.
       </p>
